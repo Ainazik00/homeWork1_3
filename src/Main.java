1b4647947ae73cbf7354d0659687d91f5798980d
Ainazik00 <ainazikkulchoroeva23@gmail.com>
2023-09-08 15:17:39 +0300

    добавила дз на сообразительность

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         double [] nums = {1.3, 4.5, -5.6, -6.2, 22.3, 4.9, -1.7, 14.3, 5.9, -45.9, 3.3, -6.1, 5.3, -1.2, -57,9 };
@@ -14,5 +16,22 @@ public class Main {
           }
         }
         System.out.println(a/b);
+
+        int[] numbers = {8,6,4,2,-3,-5,-9};
+        for (int i = 0; i < numbers.length; i++) {
+         int min = numbers [i];
+         int minid = i;
+            for (int j = i+1; j < numbers.length ; j++) {
+                if (numbers[j]<min){
+                    min=numbers [j];
+                    minid = j;
+                }
+            }
+            int scc = numbers [i];
+            numbers [i] = min ;
+            numbers [minid] = scc;
+            System.out.println(Arrays.toString(numbers));
+        }
     }
 }
+
